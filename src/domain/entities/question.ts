2e4d4cd5ed951bfa9a5d7c49f3cafff13b47b0bb
Samuel Gomes Vieira <leosgv.sv@gmail.com>
2023-08-27 22:30:01 -0300

    chore: Path aliases e Vitest globals

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,6 +1,7 @@
-import { Entity } from "../../core/entities/entity";
-import { UniqueEntityID } from "../../core/entities/unique-entity-id";
-import { Optional } from "../../core/types/optional";
+import { Entity } from "@/core/entities/entity";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { Optional } from "@/core/types/optional";
+import dayjs from "dayjs";
 import { Slug } from "./value-objects/slug";
 
 interface QuestionProps {
