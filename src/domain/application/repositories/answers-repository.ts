202580057eb0152e4242bcf149f1f3f60e8b0c05
Samuel Gomes Vieira <leosgv.sv@gmail.com>
2023-10-01 19:26:52 -0300

    feat: cloning everything

--- src/domain/application/repositories/answers-repository.ts
+++
@@ -1,5 +0,0 @@
-import { Answer } from "@/domain/enterprise/entities/answer";
-
-export interface AnswersRepository {
-  create(answer: Answer): Promise<void>;
-}
--- src/domain/application/repositories/questions-repository.ts
+++
@@ -1,6 +0,0 @@
-import { Question } from "@/domain/enterprise/entities/question";
-
-export interface QuestionsRepository {
-  create(question: Question): Promise<void>;
-  findBySlug(slug: string): Promise<Question | null>;
-}
